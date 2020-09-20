0c97ad005eabd5ad6159c7fa2f0372e32aa59082
Angel Alexander <angale22@gmail.com>
2020-09-19 21:38:16 -0500

    added functionality to api get request to readfile using fs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,8 +21,10 @@ module.exports = function(app) {
 
     app.get("/api/notes", function(req, res) {
         //Should read the db.json file and return all saved notes as JSON.
+        fs.readFileSync("./db/db.json", "utf8");
         return res.json(notesJSON);
     });
+        // return res.json(notesJSON);
 
     //API POST Request
 
@@ -40,23 +42,15 @@ module.exports = function(app) {
     //API DELETE Request
 
     app.delete("/api/notes/:id", function(req, res) {
+        //Should receive a query parameter containing the id of a note to delete.
         let id = req.params.id.toString();
-        console.log(id);
-        for (i=0; i < notesJSON.length; i++){
-            if (notesJSON[i].id == id){
-                console.log("match!");
+        for (i=0; i < notesJSON.length; i++) {
+            if (notesJSON[i].id == id) {
                 // responds with deleted note
                 res.send(notesJSON[i]);
-                // Removes the deleted note
+                // removes deleted note on click
                 notesJSON.splice(i, 1);
-                break;
             }
         }
-        //Should receive a query parameter containing the id of a note to delete.
-        //This means you'll need to find a way to give each note a unique id when it's saved.
-        //In order to delete a note, you'll need to read all notes from the db.json file,
-        //remove the note with the given id property, and then rewrite the notes to the db.json file.
-
     });
-
 };
