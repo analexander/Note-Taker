cbd56c46d9b4b1b69e3f05e7c39363cf9df502cb
Angel Alexander <angale22@gmail.com>
2020-09-19 21:46:14 -0500

    deleted unnecessary requirements

--- server.js
+++ server.js
@@ -6,7 +6,6 @@
 
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
